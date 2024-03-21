40838d8a4dc9ef3ce257fd01d61f51d5a0817c51
Anjaligaddamidi <149222107+Anjaligaddamidi@users.noreply.github.com>
2024-03-21 12:12:34 +0530

    Add files via upload

---
+++ ifexam.java
@@ -0,0 +1,13 @@
+class ifexam{
+public static void main(String[] args){
+int marks=50;
+if(marks<40)
+{
+System.out.println("you have failed in xyz subject");
+}
+else
+{
+System.out.println("congrats you have passed your exam");
+}
+}
+}
